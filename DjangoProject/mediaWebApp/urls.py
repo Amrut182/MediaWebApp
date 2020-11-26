f0fa5e72ccad61a39915c861415bd80bbb89b94d
Last Phantom <paritosh191200@gmail.com>
2020-11-26 15:24:55 +0530

    Completed intial backend work (authentication and redirections)

--- DjangoProject/mediaWebApp/urls.py
+++ DjangoProject/mediaWebApp/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 from mediaApp import views as mediaApp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',mediaApp_views.home,name='home'),
+    path('',mediaApp_views.home,name='home'),
     path('signup/',user_views.signup,name='signup'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
--- DjangoProject/users/views.py
+++ DjangoProject/users/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserLoginForm
 
 # creating a signup view
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserLoginForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account createf for {username}!')
-            return redirect('home')
+            return redirect('login')
     else:
-        form = UserCreationForm
-    form  = UserCreationForm()
+        form  = UserLoginForm()
     return render(request,'users/signup.html',{'form':form})
