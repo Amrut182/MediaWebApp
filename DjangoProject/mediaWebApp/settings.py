a422e3d57c0b78095b57a5e73cb8ca29402d55e7
ExplifiedApps <vivekmishraiit@gmail.com>
2020-12-08 17:26:58 +0000

    Added Database Model for VideoFiles Upload

--- DjangoProject/mediaWebApp/settings.py
+++ DjangoProject/mediaWebApp/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- DjangoProject/mediaWebApp/urls.py
+++ DjangoProject/mediaWebApp/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 from mediaApp import views as mediaApp_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
