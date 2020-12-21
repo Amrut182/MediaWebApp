c33db81214910f1c2fe81b345f5dce1287c665e5
Amogh Dasture <adasture@gmail.com>
2020-12-22 01:47:46 +0530

    Added local videos in the search page.

--- DjangoProject/mediaApp/views.py
+++ DjangoProject/mediaApp/views.py
@@ -1,9 +1,9 @@
 import pickle
-from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse,JsonResponse, HttpRequest
+from django.core.serializers.json import DjangoJSONEncoder
 # youtube imports
 import pprint as pp
 from googleapiclient.discovery import build
@@ -17,14 +17,17 @@ def home(request):
     return render(request,'mediaApp/home.html',)
 
 # Get Local Videos apis
-def get_videos(request):
-    if request.method == 'GET':
-        try:
-            data = list(VideoFile.objects.values())  # wrap in list(), because QuerySet is not JSON serializable
-            return JsonResponse(data, safe=False)  # or JsonResponse({'data': data})
-        except:
-            data = json.dumps([{ 'Error': 'No video with that name'}])
-            return JsonResponse(data, safe=False)  # or JsonResponse({'data': data}
+def get_videos(query):
+    try:
+        data =list(VideoFile.objects.filter(title__icontains=query).values())  # wrap in list(), because QuerySet is not JSON serializable
+        #return JsonResponse(data, safe=False)  # or JsonResponse({'data': data}
+        for q in data:
+            q['timestamp']=str(q['timestamp'])
+            q['Video_file']=q['Video_file'][9:]
+        return data
+    except:
+        data = json.dumps([{ 'Error': 'No video with that name'}])
+        return data  # or JsonResponse({'data': data}
 
 def youtube_query(request):
     query = request.POST['query']
@@ -40,21 +43,27 @@ def youtube_query(request):
                 'thumbnail': response['items'][id]['snippet']['thumbnails']['default']['url'],
                 'page_link': id,
         })
-
     # context = {'videoId' : "https://www.youtube.com/embed/" + str(videoId)}
     context = {
             'videos': videos,
             'show_more_link': "https://www.youtube.com/results?search_query="+query,
             'original_query': query,
     }
+    
+    context['api_response']=get_videos(query)
     request.session['context'] = context
     return render(request, 'mediaApp/home.html', context)
-
+    #return JsonResponse(context)
 
 def show_video(request, vid=None):
     vid = int(vid)
     context = request.session['context']
     context['current_video_id'] = "https://www.youtube.com/embed/" + context['videos'][vid]['video_id']
+    return render(request, 'mediaApp/home.html', context)
+
+def show_local_video(request, vid=None):
+    context = request.session['context']
+    context['current_api_video_id'] = vid
     return render(request, 'mediaApp/home.html', context)
 
 
